626609e1d8d6a603e97e32230d30ca9625a0e912
Brian Cruz <cruz.s.brian@gmail.com>
2019-07-12 16:16:13 -0700

    Position control working. Not full PID yet

--- outer_loop.m
+++ outer_loop.m
@@ -1,16 +1,18 @@
-function [ eta_d, control_thrust ] = outer_loop(pos_d, pos, v)
+function [ eta_d, control_thrust ] = outer_loop(psi_d, pos_d, pos, v)
 
 mass = 0.028;
-g = -9.81;
+g = [0; 0; -9.81];
 
-k_p = 2;
-k_v = 10;
+K_pos = 0.7;
+kp = 0.1;
 
-eta_d = [0; 0; pi / 6];
+v_d = K_pos * (pos_d - pos);
 
-v_d = k_p * (pos_d - pos);
+thrust = kp * (v_d - v) - mass * g;
+control_thrust = norm(thrust);
 
-thrust = k_v * (v_d - v);
-control_thrust = thrust(3) - mass * g;
+phi_d = asin((thrust(1) * sin(psi_d) - thrust(2) * cos(psi_d)) / norm(thrust));
+theta_d = atan((thrust(1) * cos(psi_d) + thrust(2) * sin(psi_d)) / thrust(3));
+eta_d = [phi_d; theta_d; psi_d];
 
 end
--- simulation.m
+++ simulation.m
@@ -5,14 +5,15 @@ function main
     omega_0 = [0; 0; 0];
     x0 = [pos_0; v_0; eta_0; omega_0];
 
-    pos_d = [0; 0; 5];
+    pos_d = [2; -3; 0];
+    psi_d = 0;
 
     options = odeset('MaxStep', 0.01);
-    [t, x] = ode15s(@(t, x) F(t, x, pos_d), [0 10], x0, options);
+    [t, x] = ode15s(@(t, x) F(t, x, pos_d, psi_d), [0 10], x0, options);
 
     subplot(2,1,1);
     plot(t, x(:, 1:3));
-    ylim([-10 10]);
+    ylim([-5, 5]);
     legend('pos_x', 'pos_y', 'pos_z');
     title('Position');
 
@@ -23,13 +24,13 @@ function main
     title('Attitude');
 end
 
-function x_dot = F(t, x, pos_d)
+function x_dot = F(t, x, pos_d, psi_d)
     pos = x(1:3);
     v = x(4:6);
     eta = x(7:9);
     omega = x(10:12);
 
-    [eta_d, control_thrust] = outer_loop(pos_d, pos, v);
+    [eta_d, control_thrust] = outer_loop(psi_d, pos_d, pos, v);
 
     control_torque = inner_loop(eta_d, eta, omega);
 
